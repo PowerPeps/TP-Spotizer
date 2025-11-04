5507d46ca0f013c65c7887474292114370d2b881
Papillon Virton <77052948+PowerPeps@users.noreply.github.com>
2025-11-04 14:14:03 +0100

    Ajout de la class artiste

---
+++ src/models/Artiste.java
@@ -0,0 +1,40 @@
+public class Artiste {
+    private String nom;
+    private String pays;
+    private double sommePercue;
+    
+    public Artiste(String nom, String pays) {
+        this.nom = nom;
+        this.pays = pays;
+        this.sommePercue = 0.0;
+    }
+    
+    public void ajouterRevenus(double montant) {
+        this.sommePercue += montant;
+    }
+    
+    public String getNom() {
+        return nom;
+    }
+    
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    
+    public String getPays() {
+        return pays;
+    }
+    
+    public void setPays(String pays) {
+        this.pays = pays;
+    }
+    
+    public double getSommePercue() {
+        return sommePercue;
+    }
+    
+    @Override
+    public String toString() {
+        return nom + " (" + pays + ") - Revenus: " + String.format("%.2f", sommePercue) + " EUR";
+    }
+}
